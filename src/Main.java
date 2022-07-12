c86f66b22c51e4eaf4114ae899f82ed0e1300246
ruslanzverkov <createmed@mail.ru>
2022-07-12 21:11:32 +0300

    1 dz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        цельночисленные
+        byte c= 2;
+        short b=3;
+        int f=5;
+        long v=10L;
+        long d = c+b+f+v;
+        System.out.println(d);
+//        с плавающей точкой
+        float n=11.11f;
+        double m=11.5;
+//        символьные переменные
+        char cc=35;
+        char ch='#';
+        System.out.println(cc+"="+ch);
+//        логические переменные
+        boolean age =10>=7;
+        boolean age2=10<=5;
+        System.out.println(age);
+        System.out.println(age2);
+
+
+
     }
 }
